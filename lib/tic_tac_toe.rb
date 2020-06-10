39d50cf3ae82e2086bb787c6b7d0bd3da878d8ba
Michael Waide <mvwaide@gmail.com>
2020-06-10 20:04:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ def position_taken? (board, index)
   end
 end
 
-def valid_move?(board, index)
+def move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
     return true
   else
